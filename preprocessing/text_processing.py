d29f6c94ac634b730cefafd4d51bb65d25ba07e8
Manuel García <manuel.garcia02@uptc.edu.co>
2021-02-20 15:27:49 -0500

    Fix alphabet

--- preprocessing/text_processing.py
+++ preprocessing/text_processing.py
@@ -10,8 +10,8 @@ _other_symbols = 'ʍwɥʜʢʡɕʑɺɧ'
 _diacrilics = 'ɚ˞ɫ'
 _phonemes = sorted(list(
     _vowels + _non_pulmonic_consonants + _pulmonic_consonants + _suprasegmentals + _other_symbols + _diacrilics))
-_punctuations = '!,-.:;? '
-_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzäüöß'
+_punctuations = '¡!,-.:;¿? '
+_alphabet = 'AÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚVWXYZaábcdeéfghiíjklmnñoópqrstuúvwxyzäüöß'
 _not_end_punctuation = ',-.:; '
 _ad_hoc_replace = {
     'Mrs.': 'Mrs',
